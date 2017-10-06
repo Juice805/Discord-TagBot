b0a86323a2ef2d09eed28b9d709220764a042338
Justin Oroz <justinoroz@me.com>
2017-10-06 01:37:54 -0700

    Add basic HTML response

--- bot.js
+++ bot.js
@@ -1,6 +1,19 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+const http = require('http');
+const fs = require('fs');
+
+http.createServer(function(req, res) {
+	data = "Bot is running!";
+	res.writeHead(200, {
+		'Content-Type': 'text/html',
+		'Content-Length': data.length
+	});
+	res.write(data);
+	res.end();
+}).listen(process.env.PORT);
+
 client.on('ready', () => {
 	console.log('I am ready!');
 });
